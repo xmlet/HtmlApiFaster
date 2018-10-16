f8e63de799c88b1d5ea9272d87fb1076ddeae73b
lcduarte <lois_duarte@msn.com>
2018-10-16 22:01:56 +0100

    * Updates HtmlApiFaster to use the HTML 5.2 specification;

--- create_class_binaries.bat
+++ create_class_binaries.bat
@@ -1,3 +1,3 @@
 if exist "./src/main/java" rmdir "./src/main/java" /s /q
 if not exist "./target/classes/org/xmlet/htmlapifaster" mkdir "./target/classes/org/xmlet/htmlapifaster"
-call mvn exec:java -D"exec.mainClass"="org.xmlet.xsdasmfaster.main.XsdAsmMain" -D"exec.args"="./src/main/resources/html_5.xsd htmlapifaster"
+call mvn exec:java -D"exec.mainClass"="org.xmlet.xsdasmfaster.main.XsdAsmMain" -D"exec.args"="./src/main/resources/html_5_2.xsd htmlapifaster"
